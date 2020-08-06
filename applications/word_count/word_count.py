a7b9e1ac4b993d6a2478f1504628f06a474929bf
dawsonhammdev <hammsdesigns@gmail.com>
2020-08-06 15:30:10 -0500

    word count is working

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,23 @@
 def word_count(s):
     # Your code here
+    # cleaning all the words from symbols and uppercase
+    for char in '":;,.-+=/\\|[]{}()*^&':
+        s=s.replace(char, '')
+    s = s.lower()
+
+    word_list = s.split()
+    
+    # initialize a dict
+    d = {}
+
+    for word in word_list:
+        # if it is a key in our dictionary we will return the value if not we will add it.
+        d[word] = d.get(word, 0) + 1
+
+    return d
+
+
+
 
 
 
